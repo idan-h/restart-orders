cbada3771949fe531c22b1ce521fd93dff21b4aa
benha <benha@monday.com>
2023-10-14 19:59:26 +0300

    added lookups python file

--- lookups.py
+++ lookups.py
@@ -1,21 +1,20 @@
 from helpers.monday_api import MondayApi, MondayBoard
-BOARD_ID = 1289515521
-
+PRODUCT_BOARD_ID = 1289515521
+ORDERS_BOARD = 1289582579
 BI_MONDAY_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI4ODIwMzA4MSwiYWFpIjoxMSwidWlkIjo1MDEyNDQ2NywiaWFkIjoiMjAyMy0xMC0xMlQwODo0OTowMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTkyNjA2MDEsInJnbiI6ImV1YzEifQ.2yE5g-JP2gcLiwBjVfMkiLZe7UZTo2vjm768yF9rcIA'
 BI_MONDAY_API_URL = 'https://api.monday.com/v2'
 
 def get_products():
     monday_api = MondayApi(BI_MONDAY_API_KEY, BI_MONDAY_API_URL)
-    monday_board = MondayBoard(monday_api, id=BOARD_ID)
+    monday_board = MondayBoard(monday_api, id=PRODUCT_BOARD_ID)
     items = monday_board.get_items(return_items_as='json').get('data').get('boards')[0].get('items')
     products = [i.get('name') for i in items ]
 
     return products
 
-
 def get_product_categories():
     monday_api = MondayApi(BI_MONDAY_API_KEY, BI_MONDAY_API_URL)
-    monday_board = MondayBoard(monday_api, id=BOARD_ID)
+    monday_board = MondayBoard(monday_api, id=PRODUCT_BOARD_ID)
     items = monday_board.get_items(return_items_as='json').get('data').get('boards')[0].get('items')
     categories = []
     for item in items:
@@ -24,11 +23,9 @@ def get_product_categories():
                 categories.append(column_value.get('text'))
     return list(set(categories))
 
-
-
 def get_products_and_categories():
     monday_api = MondayApi(BI_MONDAY_API_KEY, BI_MONDAY_API_URL)
-    monday_board = MondayBoard(monday_api, id=BOARD_ID)
+    monday_board = MondayBoard(monday_api, id=PRODUCT_BOARD_ID)
     items = monday_board.get_items(return_items_as='json').get('data').get('boards')[0].get('items')
     products_and_categories = []
     for item in items:
@@ -37,11 +34,9 @@ def get_products_and_categories():
                 products_and_categories.append([item.get('name'), column_value.get('text')])
     return products_and_categories
 
-
-
 def get_order(order_id):
     monday_api = MondayApi(BI_MONDAY_API_KEY, BI_MONDAY_API_URL)
-    monday_board = MondayBoard(monday_api, id=1291189607)
+    monday_board = MondayBoard(monday_api, id=ORDERS_BOARD)
     items = monday_board.get_items(return_items_as='json').get('data').get('boards')[0].get('items')
     for item in items:
         for column_value in item.get('column_values'):
@@ -49,6 +44,4 @@ def get_order(order_id):
                 if column_value.get('text') == str(order_id):
                     return item
     return None
-
-print(get_order(123) )
 
