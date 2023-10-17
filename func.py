007ced985912d5988e8b8f0b94b6c84222e0a5ae
Idan <ghsijns@gmail.com>
2023-10-17 16:16:35 +0300

    Added function

---
+++ func.py
@@ -0,0 +1,24 @@
+import io
+import json
+import logging
+
+from fdk import response
+
+
+def handler(ctx, data: io.BytesIO = None):
+    name = "World"
+    try:
+        request_path = ctx.http_request.path
+        logging.getLogger().info(str(request_path))
+
+        body = json.loads(data.getvalue())
+        name = body.get("name")
+    except (Exception, ValueError) as ex:
+        logging.getLogger().info('error parsing json payload: ' + str(ex))
+
+    logging.getLogger().info("Inside Python Hello World function")
+    return response.Response(
+        ctx, response_data=json.dumps(
+            {"message": "Hello {0}".format(name)}),
+        headers={"Content-Type": "application/json"}
+    )
