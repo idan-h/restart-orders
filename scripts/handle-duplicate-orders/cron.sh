d573c9782d5643736ba3dda1fb778b7606bc8df7
Idan <ghsijns@gmail.com>
2023-10-19 18:37:13 +0300

    Update cron.sh

--- scripts/handle-duplicate-orders/cron.sh
+++ scripts/handle-duplicate-orders/cron.sh
@@ -20,6 +20,9 @@ source $VENV_PATH/bin/activate
 # Update the repository
 cd $SCRIPT_PATH && git reset --hard && git pull
 
+# Ensure the cron script is executable
+chmod +x $SCRIPT_PATH/cron.sh
+
 # Install/update dependencies
 pip install -r $SCRIPT_PATH/requirements.txt
 
