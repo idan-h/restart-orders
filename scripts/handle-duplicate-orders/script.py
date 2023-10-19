cdced74b3fe576578441ce40275be6874210e7a8
Idan <ghsijns@gmail.com>
2023-10-19 18:47:50 +0300

    Update script.py

--- scripts/handle-duplicate-orders/script.py
+++ scripts/handle-duplicate-orders/script.py
@@ -1,9 +1,10 @@
-from library.functions import handle_duplicate_orders
 import os
 import sys
 
-
 sys.path.append("../../")
+
+from library.functions import handle_duplicate_orders
+
 
 API_KEY = os.environ.get('MONDAY_API_KEY')
 handle_duplicate_orders(API_KEY)
