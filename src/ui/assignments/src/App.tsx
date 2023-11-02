ce695a2a108e692e60638f98540ebc18aa132fcb
Gil Tayar <gil@tayar.org>
2023-11-02 13:25:54 +0200

    skeleton for the "edit order" page

--- src/ui/assignments/src/App.tsx
+++ src/ui/assignments/src/App.tsx
@@ -1,15 +1,26 @@
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
 import { LoginForm } from "./pages/login/LoginForm";
 import { HomePage } from "./pages/home/HomePage";
+import { EditOrder } from "./pages/edit-order/EditOrder";
 import "./App.css";
 
 function App() {
   return (
-    <FluentProvider theme={webLightTheme} dir='rtl'>
+    <FluentProvider theme={webLightTheme} dir="rtl">
       <Router>
         <Routes>
           <Route path="/login" Component={LoginForm}></Route>
+          <Route
+            path="/edit-order/:orderId"
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            Component={() => <EditOrder orderId={useParams().orderId ?? ''} />}
+          ></Route>
           <Route path="/" Component={HomePage}></Route>
         </Routes>
       </Router>
---
+++ src/ui/assignments/src/pages/edit-order/EditOrder.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const EditOrder: React.FC<{orderId: string}> = ({orderId}) => {
+  return <div>{orderId}</div>
+}
