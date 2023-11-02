0a8adbe78c3f49eff6551f742657da5db354b147
Gil Tayar <gil@tayar.org>
2023-11-02 14:52:32 +0200

    order tweaks

--- src/ui/assignments/src/pages/edit-order/EditOrder.tsx
+++ src/ui/assignments/src/pages/edit-order/EditOrder.tsx
@@ -3,13 +3,14 @@ import {
   CardHeader,
   CardPreview,
   DataGridCell,
+  Spinner,
   Table,
   TableBody,
   TableRow,
 } from "@fluentui/react-components";
 import React, { useEffect } from "react";
 import { fetchOrder } from "../../services/orders";
-import { Order } from '../../types';
+import { Order } from "../../types";
 
 export const EditOrder: React.FC<{ orderId: string }> = ({ orderId }) => {
   const [order, setOrder] = React.useState<Order | undefined>(undefined);
@@ -17,29 +18,27 @@ export const EditOrder: React.FC<{ orderId: string }> = ({ orderId }) => {
   useEffect(() => void (async () => setOrder(await fetchOrder(orderId)))(), []);
 
   return order ? (
-    <Card>
-      <CardHeader>
-        <h3>הזמנה {orderId}</h3>
-      </CardHeader>
-      <CardPreview>
-        <div>
-          עבור יחידה {order.unit} ב-{order.region}
-        </div>
-        <h4>פריטים</h4>
-        <Table>
-          <TableBody>
+    <>
+      <h2>עריכת הזמנה {orderId}</h2>
+      <div>
+        עבור יחידה <b>{order.unit}</b> ב-<b>{order.region}</b>
+      </div>
+      <h3>פריטים</h3>
+      <Table>
+        <TableBody>
+          {order.subItems.map((subItem) => (
             <TableRow>
-              {order.subItems.map((subItem) => (
-                <>
-                  <DataGridCell>{subItem.productName}</DataGridCell>
-                  <DataGridCell>{subItem.quantity}</DataGridCell>
-                  <DataGridCell>{subItem.status}</DataGridCell>
-                </>
-              ))}
+              <>
+                <DataGridCell>{subItem.productName}</DataGridCell>
+                <DataGridCell>{subItem.quantity}</DataGridCell>
+                <DataGridCell>{subItem.status}</DataGridCell>
+              </>
             </TableRow>
-          </TableBody>
-        </Table>
-      </CardPreview>
-    </Card>
-  ) : null;
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  ) : (
+    <Spinner />
+  );
 };
