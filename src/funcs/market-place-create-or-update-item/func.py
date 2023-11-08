c68e10d12d46d3ffe0ba64fdfa2fbc663b1a5729
Elad Birnboim <eladbirn@gmail.com>
2023-11-08 17:35:54 +0200

    Create item

--- src/funcs/market-place-create-or-update-item/func.py
+++ src/funcs/market-place-create-or-update-item/func.py
@@ -13,6 +13,9 @@ def handler(ctx: context, data: io.BytesIO = None):
         logger = logging.getLogger()
 
         API_KEY = ctx.Config()['API_KEY']
+        ORACLE_DSN = ctx.Config()['ORACLE_DSN']
+        ORACLE_PASSWORD = ctx.Config()['ORACLE_PASSWORD']
+        ORACLE_USER = ctx.Config()['ORACLE_USER']
 
         response_dict = {}
         logger.info(f'jsonData {jsonData}')
@@ -20,14 +23,13 @@ def handler(ctx: context, data: io.BytesIO = None):
         response_dict['itemId'] = jsonData['event']['pulseId']
         response_dict['boardId'] = jsonData['event']['boardId']
 
-        market_place_create_or_update_order(API_KEY, response_dict['itemId'], response_dict['boardId'])
-        # monday_api = MondayApi(API_KEY, API_URL)
-        # monday_board = MondayBoard(monday_api, id=response_dict['boardId'])
-        # items = monday_board.get_item_v2(response_dict['itemId']).get('data').get('items')
-        # response_dict['item'] = items
-        # logger.info(f'response {response_dict}')
-    except :
-        pass
+        market = market_place_create_or_update_order(api_key, "1308624505", "1308624313","ADMIN", "dx$D5D6p4Qg9!nk", "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.il-jerusalem-1.oraclecloud.com))(connect_data=(service_name=g47ff6ba2516085_mondaydb_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))" )
+    
+
+        market_place_create_or_update_order(API_KEY, response_dict['itemId'], response_dict['boardId'],\
+                                             ORACLE_USER, ORACLE_PASSWORD, ORACLE_DSN)
+    except (Exception,):
+        logger.info('error: ' + traceback.format_exc().replace('\n', ''))
 
     return response.Response(
         ctx, response_data=json.dumps(jsonData),
