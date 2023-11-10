64036fc4cb8e835281669b28f496703913ad31b8
Gil Epshtain <gilepshtain@microsoft.com>
2023-11-10 23:33:25 +0200

    amand

--- src/ui/assignments/src/services/orders.service.ts
+++ src/ui/assignments/src/services/orders.service.ts
@@ -1,202 +1,30 @@
-import { MondayOrder, Order } from "../types";
+import { Order, Product } from "../types";
 
 const baseUrl =
   "https://njdfolzzmvnaay5oxqife4tuwy.apigateway.il-jerusalem-1.oci.customer-oci.com/v1/";
 
-let productDetails: Map<
-  string,
-  { name: string; product_number: string; type: string }
->;
+// cache this data
+let productDetails: Map<number, Product>;
 let statusesList: string[];
 
-export function makeOrdersService(userId?: string) {
-  if (!userId) {
-    console.error("ordersService - failed to load, not logged in");
-    return null;
-  }
-
-  return {
-    /** Get all orders for the orders page */
-    async fetchUnassignedOrders(): Promise<{ orders: Order[] }> {
-      console.debug("OrdersService:fetchUnassignedOrders");
-
-      if (!productDetails) {
-        productDetails = await fetchProductDetails();
-      }
-
-      const response = await fetch(
-        new URL(
-          `get-unassigned-orders?userId=${encodeURIComponent(userId)}`,
-          baseUrl
-        )
-      );
-
-      const mondayOrders = (
-        (await response.json()) as { orders: MondayOrder[] }
-      ).orders;
-
-      return {
-        orders: mondayOrders
-          .map((mondayOrders) => ({
-            ...mondayOrders,
-            subItems: mondayOrders.subItems
-              .filter(
-                (item) => !item.userId && productDetails?.has(item.productId)
-              )
-              .map((subItem) => ({
-                ...subItem,
-                product: productDetails!.get(subItem.productId)!,
-              })),
-          }))
-          .filter((order) => order.subItems.length),
-      };
-    },
-    /** Get all orders for the my-orders page */
-    async fetchAssignedOrders(): Promise<{ orders: Order[] }> {
-      console.debug("OrdersService:fetchAssignedOrders");
-
-      if (!productDetails) {
-        productDetails = await fetchProductDetails();
-      }
-
-      const response = await fetch(
-        new URL(
-          `get-assigned-orders?userId=${encodeURIComponent(userId)}`,
-          baseUrl
-        )
-      );
-
-      const mondayOrders = (
-        (await response.json()) as { orders: MondayOrder[] }
-      ).orders;
-
-      return {
-        orders: mondayOrders
-          .map((mondayOrders) => ({
-            ...mondayOrders,
-            subItems: mondayOrders.subItems
-              .filter(
-                (item) => item.userId && productDetails?.has(item.productId)
-              )
-              .map((subItem) => ({
-                ...subItem,
-                product: productDetails!.get(subItem.productId)!,
-              })),
-          }))
-          .filter((order) => order.subItems.length),
-      };
-    },
-    /** no in use */
-    async fetchOrder(orderId: string): Promise<Order | undefined> {
-      console.debug("OrdersService:fetchOrder");
-
-      if (!productDetails) productDetails = await fetchProductDetails();
-
-      const response = await fetch(
-        new URL(
-          `get-user-order/${encodeURIComponent(
-            orderId
-          )}?userId=${encodeURIComponent(userId)}`,
-          baseUrl
-        )
-      );
-
-      const mondayOrder = (await response.json()).order as MondayOrder;
-
-      return {
-        ...mondayOrder,
-        subItems: mondayOrder.subItems.map((subItem) => ({
-          ...subItem,
-          productName: productDetails!.get(subItem.productId)!,
-        })),
-      } as Order;
-    },
-    /** Move item from orders to my-orders  */
-    async assignSubItem(request: {
-      orderId: string;
-      subItemId: string;
-      subItemBoardId: string;
-    }) {
-      console.debug("OrdersService:assignSubItem");
-
-      const response = await fetch(
-        new URL(`assign?userId=${encodeURIComponent(userId)}`, baseUrl),
-        { method: "POST", body: JSON.stringify(request) }
-      );
-
-      await response.json();
-    },
-    /** (Delete) Move item from my-orders to orders  */
-    async unAssignSubItem(request: {
-      orderId: string;
-      subItemId: string;
-      subItemBoardId: string;
-    }) {
-      console.debug("OrdersService:unAssignSubItem");
-
-      const response = await fetch(
-        new URL(`unassign?userId=${encodeURIComponent(userId)}`, baseUrl),
-        { method: "POST", body: JSON.stringify(request) }
-      );
-
-      await response.json();
-    },
-    /** Get all statuses for the status dropdown  */
-    async fetchOrderStatusNames(): Promise<string[]> {
-      console.debug("OrdersService:fetchOrderStatusNames");
-
-      if (statusesList) return statusesList;
-
-      const response = await fetch(
-        new URL(
-          `get-subitem-statuses?userId=${encodeURIComponent(userId)}`,
-          baseUrl
-        )
-      );
-
-      return (await response.json()).statuses.filter(
-        (status: string) => Boolean(status) // filter empty
-      );
-    },
-    /** Change item status  */
-    async changeStatus(request: {
-      orderId: string;
-      subItemId: string;
-      subItemBoardId: string;
-      status: string;
-    }) {
-      console.debug("OrdersService:changeStatus");
-
-      const response = await fetch(
-        new URL(`change-status?userId=${encodeURIComponent(userId)}`, baseUrl),
-        { method: "POST", body: JSON.stringify(request) }
-      );
-
-      await response.json();
-    },
-  };
+export class OrdersService {
+  constructor(private _userId: string) {}
 
   /** List of all items in the database. */
-  async function fetchProductDetails(): Promise<
-    Map<string, { name: string; product_number: string; type: string }>
-  > {
-    if (!userId) {
-      console.error("ordersService - failed to load, not logged in");
-      return Promise.reject();
-    }
-
+  private async _fetchProductDetails(): Promise<Map<number, Product>> {
     console.debug("OrdersService:fetchProductNames");
 
     const response = await fetch(
-      new URL("get-products?userId=" + encodeURIComponent(userId), baseUrl)
+      new URL(
+        "get-products?userId=" + encodeURIComponent(this._userId),
+        baseUrl
+      )
     );
 
-    const mondayProducts = await response.json();
+    const backendProducts = await response.json();
 
-    // map of number -> key = "product_number", value = "product_number", "name", "type"}
     return new Map(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      mondayProducts.map((product: any) => [
+      backendProducts.map((product: Product) => [
         product.product_number,
         {
           name: product.name,
@@ -205,5 +33,167 @@ export function makeOrdersService(userId?: string) {
         },
       ])
     );
+  }
+
+  /** Get all orders for the orders page */
+  public async fetchUnassignedOrders(): Promise<{ orders: Order[] }> {
+    console.debug("OrdersService:fetchUnassignedOrders");
+
+    if (!productDetails) productDetails = await this._fetchProductDetails();
+
+    const response = await fetch(
+      new URL(
+        `get-unassigned-orders?userId=${encodeURIComponent(this._userId)}`,
+        baseUrl
+      )
+    );
+
+    const backendOrders = ((await response.json()) as { orders: Order[] })
+      .orders;
+
+    return {
+      orders: backendOrders
+        .map((order) => ({
+          ...order,
+          subItems: order.subItems
+            .filter(
+              (item) => !item.userId && productDetails?.has(item.productId)
+            )
+            .map((subItem) => ({
+              ...subItem,
+              product: productDetails!.get(subItem.productId)!,
+            })),
+        }))
+        .filter((order) => order.subItems.length),
+    };
+  }
+
+  /** Get all orders for the my-orders page */
+  public async fetchAssignedOrders(): Promise<{ orders: Order[] }> {
+    console.debug("OrdersService:fetchAssignedOrders");
+
+    if (!productDetails) productDetails = await this._fetchProductDetails();
+
+    const response = await fetch(
+      new URL(
+        `get-assigned-orders?userId=${encodeURIComponent(this._userId)}`,
+        baseUrl
+      )
+    );
+
+    const backendOrders = ((await response.json()) as { orders: Order[] })
+      .orders;
+
+    return {
+      orders: backendOrders
+        .map((order) => ({
+          ...order,
+          subItems: order.subItems
+            .filter(
+              (item) => item.userId && productDetails?.has(item.productId)
+            )
+            .map((subItem) => ({
+              ...subItem,
+              product: productDetails!.get(subItem.productId)!,
+            })),
+        }))
+        .filter((order) => order.subItems.length),
+    };
+  }
+
+  /** no in use */
+  public async fetchOrder(orderId: string): Promise<Order> {
+    console.debug("OrdersService:fetchOrder");
+
+    if (!productDetails) productDetails = await this._fetchProductDetails();
+
+    const response = await fetch(
+      new URL(
+        `get-user-order/${encodeURIComponent(
+          orderId
+        )}?userId=${encodeURIComponent(this._userId)}`,
+        baseUrl
+      )
+    );
+
+    const backendOrder = (await response.json()).order as Order;
+
+    return {
+      ...backendOrder,
+      subItems: backendOrder.subItems.map((subItem) => ({
+        ...subItem,
+        productName: productDetails!.get(subItem.productId)!,
+      })),
+    };
+  }
+
+  /** Move item from orders to my-orders  */
+  public async assignSubItem(request: {
+    orderId: number;
+    subItemId: number;
+    subItemBoardId: number;
+  }) {
+    console.debug("OrdersService:assignSubItem");
+
+    const response = await fetch(
+      new URL(`assign?userId=${encodeURIComponent(this._userId)}`, baseUrl),
+      { method: "POST", body: JSON.stringify(request) }
+    );
+
+    await response.json();
+  }
+
+  /** (Delete) Move item from my-orders to orders  */
+  public async unAssignSubItem(request: {
+    orderId: number;
+    subItemId: number;
+    subItemBoardId: number;
+  }) {
+    console.debug("OrdersService:unAssignSubItem");
+
+    const response = await fetch(
+      new URL(`unassign?userId=${encodeURIComponent(this._userId)}`, baseUrl),
+      { method: "POST", body: JSON.stringify(request) }
+    );
+
+    await response.json();
+  }
+
+  /** Get all statuses for the status dropdown  */
+  public async fetchOrderStatusNames(): Promise<string[]> {
+    console.debug("OrdersService:fetchOrderStatusNames");
+
+    if (statusesList) return statusesList;
+
+    const response = await fetch(
+      new URL(
+        `get-subitem-statuses?userId=${encodeURIComponent(this._userId)}`,
+        baseUrl
+      )
+    );
+
+    return (await response.json()).statuses.filter(
+      (status: string) => Boolean(status) // filter empty
+    );
+  }
+
+  /** Change item status  */
+  public async changeStatus(request: {
+    orderId: number;
+    subItemId: number;
+    subItemBoardId: number;
+    status: string;
+  }) {
+    console.debug("OrdersService:changeStatus");
+
+    const response = await fetch(
+      new URL(
+        `change-status?userId=${encodeURIComponent(this._userId)}`,
+        baseUrl
+      ),
+      { method: "POST", body: JSON.stringify(request) }
+    );
+
+    await response.json();
   }
 }
