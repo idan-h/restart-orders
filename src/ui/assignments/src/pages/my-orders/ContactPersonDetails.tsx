99143fb9f5834ca22afb1c9e2fc29aaa83fb7fa6
ruthchemla <ruth.shemla@gmail.com>
2023-11-10 15:46:03 +0200

    Revert "unit before name"
    This reverts commit 44ff92618ce448043f6ae3b0fe57743fadb7a27a.

--- src/ui/assignments/src/pages/my-orders/ContactPersonDetails.tsx
+++ src/ui/assignments/src/pages/my-orders/ContactPersonDetails.tsx
@@ -14,15 +14,6 @@ import { ContactPersonDetails } from "../../types";
 
 const columns: TableColumnDefinition<ContactPersonDetails>[] = [
   createTableColumn<ContactPersonDetails>({
-    columnId: "unit",
-    renderHeaderCell: () => {
-      return "יחידה";
-    },
-    renderCell: (item) => {
-      return <TableCellLayout>{item.unit}</TableCellLayout>;
-    },
-  }),
-  createTableColumn<ContactPersonDetails>({
     columnId: "name",
     renderHeaderCell: () => {
       return "שם";
@@ -31,7 +22,15 @@ const columns: TableColumnDefinition<ContactPersonDetails>[] = [
       return <TableCellLayout>{item.name}</TableCellLayout>;
     },
   }),
-
+  createTableColumn<ContactPersonDetails>({
+    columnId: "unit",
+    renderHeaderCell: () => {
+      return "יחידה";
+    },
+    renderCell: (item) => {
+      return <TableCellLayout>{item.unit}</TableCellLayout>;
+    },
+  }),
   createTableColumn<ContactPersonDetails>({
     columnId: "region",
 
